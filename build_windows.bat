988b19fff4eaf13d7f972e4bf3997c3f61dbe66d
Michel de Bree <michel@micheldebree.nl>
2020-11-09 06:50:34 +0100

    Make sure action fails if Windows build has an error.

--- build_windows.bat
+++ build_windows.bat
@@ -1,21 +1,37 @@
 MSBuild.exe SIDFactoryII.sln -property:Configuration=Release -property:Platform=x86
+IF ERRORLEVEL 1 EXIT 1
 
 md artifacts
+IF ERRORLEVEL 1 EXIT 1
 md artifacts\color_schemes
+IF ERRORLEVEL 1 EXIT 1
 md artifacts\drivers
+IF ERRORLEVEL 1 EXIT 1
 md artifacts\music
+IF ERRORLEVEL 1 EXIT 1
 md artifacts\overlay
+IF ERRORLEVEL 1 EXIT 1
 md artifacts\documentation
+IF ERRORLEVEL 1 EXIT 1
 
 copy /Y release\SIDFactoryII.exe artifacts\
+IF ERRORLEVEL 1 EXIT 1
 copy /Y release\SF2Converter.exe artifacts\
+IF ERRORLEVEL 1 EXIT 1
 
 copy /Y SIDFactoryII\config.ini artifacts\
+IF ERRORLEVEL 1 EXIT 1
 copy /Y SIDFactoryII\COPYING artifacts\
+IF ERRORLEVEL 1 EXIT 1
 copy /Y SIDFactoryII\SDL2.dll artifacts\
+IF ERRORLEVEL 1 EXIT 1
 
 copy /Y SIDFactoryII\color_schemes\*.* artifacts\color_schemes\
+IF ERRORLEVEL 1 EXIT 1
 copy /Y SIDFactoryII\drivers\*.* artifacts\drivers\
+IF ERRORLEVEL 1 EXIT 1
 copy /Y SIDFactoryII\music\*.* artifacts\music\
+IF ERRORLEVEL 1 EXIT 1
 copy /Y SIDFactoryII\overlay\*.* artifacts\overlay\
+IF ERRORLEVEL 1 EXIT 1
 copy /Y dist\documentation\*.* artifacts\documentation\
